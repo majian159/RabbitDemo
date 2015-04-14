3f944226a6379aefcb6da0ac7a6bbc2b799e0520
majian <majian159@live.com>
2015-04-14 16:39:40 +0800

    1.更新packages。 2.添加账号登录的功能。

---
+++ Rabbit.MVC/Web/Modules/Rabbit.UserInterface/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using Rabbit.Components.Security;
+using Rabbit.UserInterface.ViewModels;
+using System.Web.Mvc;
+
+namespace Rabbit.UserInterface.Controllers
+{
+    public class AccountController : Controller
+    {
+        #region Field
+
+        private readonly IAuthenticationService _authenticationService;
+
+        #endregion Field
+
+        #region Constructor
+
+        public AccountController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        #endregion Constructor
+
+        #region Action
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        [ActionName("SignIn")]
+        [HttpPost]
+        public ActionResult SignInPost(SignInViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            _authenticationService.SignIn(new UserModel { Identity = model.UserName, UserName = model.UserName }, model.Remember);
+            return RedirectToAction("Index", "Admin");
+        }
+
+        public ActionResult SignOut()
+        {
+            _authenticationService.SignOut();
+            return RedirectToAction("SignIn");
+        }
+
+        #endregion Action
+    }
+}
